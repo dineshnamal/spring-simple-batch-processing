7ec4e8533726baa093fdeabd3edd2aa32a98fa9a
Namal Kankanamalage <namal@tramada.com>
2021-05-30 00:25:10 +1000

    Batch processign initial commit

---
+++ src/test/java/com/namal/batch/processing/batchprocessing/BatchProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namal.batch.processing.batchprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
