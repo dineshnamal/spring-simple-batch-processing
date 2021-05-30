bbb4f9afadbe9f21f8ab85ec9f284d5440e55a09
Namal Kankanamalage <namal@tramada.com>
2021-05-30 18:07:17 +1000

    unit test folder added

---
+++ src/test/java/com/namal/batch/processing/BatchProcessingApplicationTests.java
@@ -0,0 +1,19 @@
+package com.namal.batch.processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	
+	@Test 
+	public void testEcho() {
+		
+	}
+
+}
