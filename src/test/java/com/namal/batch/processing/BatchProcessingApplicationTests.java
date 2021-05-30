f97245f05b3d928d823e50bac129ef652902a859
Namal Kankanamalage <namal@tramada.com>
2021-05-30 18:37:29 +1000

    add listener

--- src/test/java/com/namal/batch/processing/BatchProcessingApplicationTests.java
+++ src/test/java/com/namal/batch/processing/BatchProcessingApplicationTests.java
@@ -2,7 +2,9 @@ package com.namal.batch.processing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class })
 @SpringBootTest
 class BatchProcessingApplicationTests {
 
